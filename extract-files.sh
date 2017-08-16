72ae205e79f16292959627b0e3b81179501dc05c
Matthias Yzusqui <myzb.dev@gmail.com>
2017-08-15 22:48:02 -0400

    condor: Update extraction scripts
    * Kanged from titan
    * Meant to work with updated scripts in common
    
    Change-Id: Ic5e4d44512fec9a250c4903ada0aa07f70b71436

--- extract-files.sh
+++ extract-files.sh
@@ -1,58 +1,27 @@
 #!/bin/bash
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Use Traditional sorting
-export LC_ALL=C
+set -e
 
-FP=$(cd ${0%/*} && pwd -P)
-export VENDOR=$(basename $(dirname $FP))
-export DEVICE=$(basename $FP)
+# Required!
+export DEVICE=condor
+export DEVICE_COMMON=msm8610-common
+export VENDOR=motorola
 
-while getopts ":hd:" options
-do
-    case $options in
-        d ) LDIR=$OPTARG ;;
-        h ) echo "Usage: `basename $0` [OPTIONS] "
-            echo "  -d  Fetch blobs from local directory"
-            echo "  -h  Show this help"
-            exit ;;
-        * ) ;;
-    esac
-done
+export DEVICE_BRINGUP_YEAR=2014
 
-function extract() {
-    for FILE in `egrep -v '(^#|^$)' $1`; do
-        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-        DEST=${PARSING_ARRAY[1]}
-        if [ -z $DEST ]; then
-            DEST=$FILE
-        fi
-        DIR=`dirname $DEST`
-        if [ ! -d $2/$DIR ]; then
-            mkdir -p $2/$DIR
-        fi
-        if [ -z $LDIR ]; then
-            # Try CM target first
-            adb pull /system/$DEST $2/$DEST
-            # if file does not exist try OEM target
-            if [ "$?" != "0" ]; then
-                adb pull /system/$FILE $2/$DEST
-            fi
-        else
-            # Try CM target first
-            cp $LDIR/system/$DEST $2/$DEST
-            # if file does not exist try OEM target
-            if [ "$?" != "0" ]; then
-                cp $LDIR/system/$FILE $2/$DEST
-            fi
-        fi
-    done
-}
-
-
-DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVBASE/*
-
-extract proprietary-files.txt $DEVBASE
-
-../../../device/$VENDOR/$DEVICE/setup-makefiles.sh
+./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
